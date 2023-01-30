a9b88af3ec661603c5fdd7ba384a94ee2d22c84a
Florian Klampfer <mail@qwtel.com>
2023-01-30 16:08:57 +0700

    Commit

--- worker.js
+++ worker.js
@@ -1,15 +1,6 @@
 import noSupport from './worker-no-support.js';
 import support from './worker-support.js';
 
-/**
- * Because it's not possible to check support for nested workers in the window
- * scope, message handling must be overriden regardless. Creation of workers is
- * however left to the native implementation when possible.
- */
-const supportsNestedWorker = typeof Worker === 'function';
-
-if (supportsNestedWorker) {
-  support();
-} else {
+if (navigator.userAgent.includes("WebKit/") && !navigator.userAgent.includes("Chrome/")) {
   noSupport();
 }
